ffef16841957fe45a04a5ee4e1230c15171bc2d2
ashdroidcmd <ashgesite25@gmail.com>
2025-04-04 19:13:49 +0800

    Fix image path

--- src/components/SideContent.jsx
+++ src/components/SideContent.jsx
@@ -12,7 +12,7 @@ return (
 
                   <Link className="aside-custom d-flex flex-column flex-lg-row align-items-start align-items-lg-center my-3 text-decoration-none gap-2" to={`/posts/${posts.slug}`}>
                         <div className="col-lg-6">
-                        <img className="img-fluid rounded-3" src={`/images/${posts.image[0]}`} alt={posts.title}/>
+                        <img className="img-fluid rounded-3" src={`/droid-blog-website/images/${posts.image[0]}`} alt={posts.title}/>
                         </div>
                         <div className="col-lg-6">
                           <h6 className="mb-0 text-white ">{posts.title}</h6>
