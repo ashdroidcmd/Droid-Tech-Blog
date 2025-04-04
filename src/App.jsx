b0d60a8965626ae386e4272e755368f8b1f0e480
ashdroidcmd <ashgesite25@gmail.com>
2025-04-04 17:55:56 +0800

    Fixed routing issue for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
-import ProjectsPage from './pages/ProjectPage'
+import ProjectsPage from './pages/ProjectPage';
 import AboutPage from './pages/AboutPage';
 import ResourcesPage from './pages/ResourcesPage';
 import Postpage from './pages/Postpage';
 
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-  <Route path='/' element={<MainLayout />}>
-    <Route index element={<HomePage />} />
-    <Route path='/projects' element={<ProjectsPage />} />
-    <Route path='/about' element={<AboutPage />} />
-    <Route path='/resources' element={<ResourcesPage />} />
-    <Route path='/posts/:slug' element={<Postpage />} />
-  </Route>
-)
-  
-);
-
 function App() {
-  return <RouterProvider router={router} />;
-};
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/resources" element={<ResourcesPage />} />
+          <Route path="/posts/:slug" element={<Postpage />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
