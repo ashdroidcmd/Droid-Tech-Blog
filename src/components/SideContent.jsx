e092ea9527d7b153734b6ce5d5f8c4a1ee5abe9f
ashdroidcmd <z.wolff25@gmail.com>
2025-05-02 13:23:24 +0800

    fix image path

--- src/components/SideContent.jsx
+++ src/components/SideContent.jsx
@@ -28,7 +28,7 @@ const SideContent = () => {
             <Link className="aside-custom d-flex flex-column flex-lg-row align-items-start align-items-lg-center my-3 text-decoration-none gap-2" to={`/posts/${project.slug}`}>
               <div className="col-lg-6">
 
-                <img className="img-fluid rounded-3" src={`${import.meta.env.BASE_URL}/images/${project.image[0]}`} alt={project.title} />
+                <img className="img-fluid rounded-3" src={`${import.meta.env.BASE_URL}images/${project.image[0]}`} alt={project.title} />
               </div>
               <div className="col-lg-6">
                 <p className="mb-0 text-white">{project.title}</p>
